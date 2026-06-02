51ddbb9ad14e421cefd610230df606f918b7d395
sanika-techg <sanika.techg@gmail.com>
2026-06-02 06:45:16 +0000

    conversion operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -14,5 +14,29 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//operations*******************************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " sanika"
+
+let str3 = str1+str2
+console.log(str3);
+
+
+//console.log("1" + 2);
+//console.log(+true);
+//console.log(-"");
+
+let counter = 100
+counter++
+console.log(counter);
+
+
+
+
+
 
 
